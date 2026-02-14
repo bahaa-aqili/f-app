37a8b7c7aa47763dfe3390a486794dca616d0547
bahaa-aqili <bahaa.aqili92@gmail.com>
2026-02-14 14:19:32 +0300

    next icon

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["i.pravatar.cc"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.pravatar.cc",
+      },
+      {
+        protocol: "https",
+        hostname: "www.svgrepo.com",
+      },
+    ],
   },
 };
 
